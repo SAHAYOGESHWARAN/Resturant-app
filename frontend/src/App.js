789035860076234098e0988972665e466b40d123
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-06 22:51:11 +0530

    RA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Orders from './pages/Orders';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/orders" component={Orders} />
       </Switch>
     </Router>
   );
