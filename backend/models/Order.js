d455126e7e9014dc4cee054acb5b126a63e762f4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-06 22:45:11 +0530

    RA

---
+++ backend/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
+  items: [
+    {
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalPrice: { type: Number, required: true },
+  status: { type: String, default: 'Pending' },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
